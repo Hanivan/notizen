9906170adbbf6d970722ed618c1bfae665f6d18d
アニップ <hanivan20@gmail.com>
2024-12-13 20:55:43 +0700

    bump: use react instead of preact. add shadcn/ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,19 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hanivan.my.id",
-  integrations: [mdx(), sitemap(), tailwind(), preact()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      // applyBaseStyles: true,
+    }),
+    react(),
+  ],
 });
