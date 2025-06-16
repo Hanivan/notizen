a78cdaf432b808b973a7f494ca9530df64b0e021
アニップ <Hanivan@users.noreply.github.com>
2025-06-16 16:54:11 +0700

    chore: .

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			// Include content files in the serverless functions
+			external: [],
+			split: false
+		})
+	},
 	extensions: ['.svelte', '.svx']
 };
 
